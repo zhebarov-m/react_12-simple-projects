f6fba873f05b0df71af34cabc65ac055e3fa2544
Muhammad Zhebarov <xarcho.ev@gmail.com>
2023-09-05 22:50:23 +0300

    added funtcional control input

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
-import { FC, useEffect, useState } from 'react'
-import { Users } from './components/Users';
+import { FC, useEffect, useState } from "react";
+import { Users } from "./components/Users";
 // import { Success } from './components/Success';
-import axios from 'axios';
+import axios from "axios";
 
 // Тут список пользователей: https://reqres.in/api/users
 
-const App:FC = () => {
-  const [users, setUsers] = useState([])
-  const [isLoading, setLoading] = useState(true)
+const App: FC = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [invites, setInvites] = useState([]);
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
 
   useEffect(() => {
     const FetchData = async () => {
       try {
-        const {data} = await axios.get('https://reqres.in/api/users')
-        setUsers(data.data)
+        const { data } = await axios.get("https://reqres.in/api/users");
+        setUsers(data.data);
       } catch (error) {
-        console.error('Ошибка при запросе:', error);
-        
+        console.error("Ошибка при запросе:", error);
       }
-    }
+    };
 
-    FetchData()
-    setLoading(false)
-  }, [])
+    FetchData();
+    setLoading(false);
+  }, []);
 
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading}/>
+      <Users
+        searchValue={searchValue}
+        onChangeValue={handleChangeInput}
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
   );
-}
+};
 
-export default App
-
+export default App;
--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,30 +1,36 @@
-
-import { FC } from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import { FC } from "react";
+import { Skeleton } from "./Skeleton";
+import { User } from "./User";
 
 export type tUsersData = {
-  id: number,
-  email: string,
-  first_name: string,
-  last_name: string,
-  avatar: string
-}
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+};
 
 interface iUsersProps {
-  items: tUsersData[],
-  isLoading: boolean,
+  items: tUsersData[];
+  isLoading: boolean;
+  searchValue: string;
+  onChangeValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Users:FC<iUsersProps> = (props) => {
-  const { items, isLoading } = props
+export const Users: FC<iUsersProps> = (props) => {
+  const { items, isLoading, searchValue, onChangeValue } = props;
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          value={searchValue}
+          onChange={onChangeValue}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -34,7 +40,14 @@ export const Users:FC<iUsersProps> = (props) => {
         </div>
       ) : (
         <ul className="users-list">
-          {items.map((item) => (<User key={item.id} userData={item}/>))}
+          {items
+            .filter((item) => {
+              const fullName = `${item.first_name} ${item.last_name}`;
+              return fullName.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((item) => (
+              <User key={item.id} userData={item} />
+            ))}
         </ul>
       )}
       <button className="send-invite-btn">Отправить приглашение</button>
